a6d2c76d767d17736896d05838e11aa7bd6d1a90
elliotgaramendi <elliotgaramendi@gmail.com>
2026-03-03 11:19:16 -0500

    feat: add ModulesHero component schema & json (n-s)

--- next16-strapi5/backend/types/generated/components.d.ts
+++ next16-strapi5/backend/types/generated/components.d.ts
@@ -1,3 +1,23 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface ModulesHero extends Struct.ComponentSchema {
+  collectionName: 'components_modules_heroes';
+  info: {
+    displayName: 'Hero';
+    icon: 'grid';
+  };
+  attributes: {
+    heading: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images' | 'files'> &
+      Schema.Attribute.Required;
+    subheading: Schema.Attribute.Text;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'modules.hero': ModulesHero;
+    }
+  }
+}
