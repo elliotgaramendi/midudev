f2a93bd36f997f6a380d562d0915d0c4f7e2fd31
elliotgaramendi <elliotgaramendi@gmail.com>
2026-03-08 19:37:20 -0500

    feat: implemente login user logic (n-s)

--- next16-strapi5/frontend/actions/auth.ts
+++ next16-strapi5/frontend/actions/auth.ts
@@ -4,8 +4,8 @@ import { z } from "zod"
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 
-import { registerUserService } from "@/lib/strapi"
-import { SignupFormSchema, type FormState } from "@/validations/auth"
+import { loginUserService, registerUserService } from "@/lib/strapi"
+import { SigninFormSchema, SignupFormSchema, type FormState } from "@/validations/auth"
 
 const cookieConfig = {
   maxAge: 60 * 60 * 24,
@@ -46,6 +46,47 @@ export async function registerUserAction(prevState: FormState, formData: FormDat
     return {
       success: false,
       message: "Registration error",
+      strapiErrors: response?.error,
+      zodErrors: null,
+      data: fields
+    }
+  }
+
+  const cookieStore = await cookies()
+  cookieStore.set('jwt', response.jwt, cookieConfig)
+  redirect('/dashboard')
+}
+
+export async function loginUserAction(prevState: FormState, formData: FormData): Promise<FormState> {
+  console.log('Hello from Login User Action')
+
+  const fields = {
+    identifier: formData.get('identifier') as string,
+    password: formData.get('password') as string,
+  }
+
+  const validatedFields = SigninFormSchema.safeParse(fields)
+
+  if (!validatedFields.success) {
+    const flattenedErrors = z.flattenError(validatedFields.error)
+
+    console.log("Validation errors:", flattenedErrors.fieldErrors)
+
+    return {
+      success: false,
+      message: "Validation error",
+      strapiErrors: null,
+      zodErrors: flattenedErrors.fieldErrors,
+      data: fields
+    }
+  }
+
+  const response = await loginUserService(validatedFields.data)
+
+  if (!response || response.error) {
+    return {
+      success: false,
+      message: "Login error",
       strapiErrors: response?.error,
       zodErrors: null,
       data: fields
